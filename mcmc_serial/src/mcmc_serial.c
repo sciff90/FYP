c85b00508c4be5be2dc0aa2680e24f328833f5b7
Gareth Sciffer <gareths@mds041-OptiPlex-780.(none)>
2013-08-09 13:17:57 +1000

    added mcmc_serial

---
+++ mcmc_serial/src/mcmc_serial.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+int main(int argc, char *argv[])
+{
+	const gsl_rng_type * T;
+	gsl_rng * r;
+
+	//select random number generator
+	r = gsl_rng_alloc(gsl_rng_mt19937);
+	FILE *fp;
+
+	const int N = 1000,Iters = 1000;
+	int ii;
+
+	double x_0,alpha,candidate,a;
+
+	fp = fopen("./data/test.dat","w");
+	//generate random starting point
+	x_0 = gsl_ran_flat(r,-20,20);
+	alpha = 0.25;
+
+	for (ii = 0; ii < Iters; ii++)
+	{
+		candidate = x_0+gsl_ran_gaussian(r,alpha);
+		a = gsl_ran_ugaussian_pdf(candidate)/gsl_ran_ugaussian_pdf(x_0);
+		//printf("a = %f\n",a);
+		if(gsl_rng_uniform(r)<a)
+			x_0 = candidate;
+		fprintf(fp,"%f\n",x_0);
+
+	}
+	fclose(fp);
+	gsl_rng_free(r);
+	system("gnuplot mcmc_serial.plt");
+
+
+	return 0;
+}
